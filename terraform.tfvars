2d3f686f846be2190d2d3e78342648ee45919572
santhosh9680 <santhoshsathwik@gmail.com>
2024-05-01 04:37:19 +0000

    first

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-04b70fa74e45c3917"
+instance_type_value = "t2.micro"
+subnet_id_value = "subnet-0652756769f6ef5e7"
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami_value" {
+  description = "value for the ami"
+}
+ variable "instance_type_value" {
+   description = "value of instance_type"
+ }
+
+variable "subnet_id_value" {
+  description = " value for the subnet_id"
+}
